0f5d3bfb4b46934de62f0c005671a4a4e098915b
catalandu <84291747+catalandu@users.noreply.github.com>
2021-11-05 20:28:49 +0100

    Add files via upload

---
+++ LifeR/README.md
@@ -0,0 +1,13 @@
+# LoadingScreen-v3
+
+Bonjour 
+
+je vous partage mon ecran de chargement, fait pour mon serveur LifeR. (plus d'actualité) 
+Donc je vous poste ça sur github, si vous avez des soucis ouvrez une issue.
+Fait avec Boostrap. Ca changera des écrans de chargement similaires..
+
+Demo : https://curly-grass-5800.bss.design/
+
+Merci de me créditer si vous l'utilisez.
+
+dlaV#0001
---
+++ LifeR/__resource.lua
@@ -0,0 +1,19 @@
+description 'Pola mare v1'
+
+files {
+    --Loading Index File--
+    'index.html',
+    '/assets/img/1.png',
+    '/assets/img/4.png',
+    '/assets/css/Animated-Typing-With-Blinking.css',
+    '/assets/css/Footer-Dark.css',
+    '/assets/css/Highlight-Phone.css',
+    '/assets/css/styles.css',
+    '/assets/js/Animated-Typing-With-Blinking.js',
+    '/assets/js/jquery.min.js',
+    '/assets/bootstrap/css/bootstrap.min.css',
+    '/assets/bootstrap/js/bootstrap.min.js',
+
+}
+
+loadscreen 'index.html'
